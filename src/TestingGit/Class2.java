cac340985e5a7e39b5d5925482e6bc875a5825ac
erezka <soretatosha@gmail.com>
2019-07-03 02:32:57 +0400

    добавили в Class2 вывод роста

--- src/TestingGit/Class2.java
+++ src/TestingGit/Class2.java
@@ -4,6 +4,8 @@ public class Class2 {
     public static void main(String[] args) {
         human hero = new human ("Ivan",22);
         System.out.println(hero.getAge());
+        hero.setHeight(180);
+        System.out.println(hero.getHeight());
     }
 
 
