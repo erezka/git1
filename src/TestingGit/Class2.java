0b381dced3033f8d6c2081990baea3a5968c7eb6
erezka <soretatosha@gmail.com>
2019-07-03 01:48:15 +0400

    создали объект в Class2

--- src/TestingGit/Class2.java
+++ src/TestingGit/Class2.java
@@ -1,4 +1,10 @@
 package TestingGit;
 
 public class Class2 {
+    public static void main(String[] args) {
+        human hero = new human ("Ivan",22);
+        System.out.println(hero.getAge());
+    }
+
+
 }
