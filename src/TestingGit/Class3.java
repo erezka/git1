4f1bed389a0c95463029c148e6e85b8f8a8384c2
erezka <soretatosha@gmail.com>
2019-07-03 23:33:16 +0400

    делаем commit изменений из Shelf

---
+++ src/TestingGit/Class3.java
@@ -0,0 +1,12 @@
+package TestingGit;
+
+public class Class3 {
+    public static void main(String[] args) {
+        int a;
+        System.out.println("я создал новый класс!");
+        int b = 1;
+        a = b;
+        System.out.println(a);
+
+    }
+}
